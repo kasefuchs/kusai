02b873ce4ef82b0c84f90d54e119ad5395e8e715
kasefuchs <kasefuchs@protonmail.com>
2025-10-19 22:03:50 +0300

    chore: add markov chain

--- src/graph/Graph.hpp
+++ src/graph/Graph.hpp
@@ -13,7 +13,21 @@ public:
 
   graph::Node *addNode();
 
+  [[nodiscard]] graph::Node *getNode(uint32_t id) const;
+
+  graph::Edge *addEdge(uint32_t source, uint32_t target);
+
   graph::Edge *addEdge(const graph::Node *source, const graph::Node *target);
+
+  [[nodiscard]] graph::Edge *getEdge(uint32_t source, uint32_t target) const;
+
+  graph::Edge *getEdge(const graph::Node *source, const graph::Node *target) const;
+
+  std::vector<graph::Edge *> getOutgoingEdges(const graph::Node *source) const;
+
+  graph::Edge *getOrAddEdge(uint32_t source, uint32_t target);
+
+  graph::Edge *getOrAddEdge(const graph::Node* source, const graph::Node* target);
 
   void serialize(graph::Graph *out) const;
 
@@ -24,6 +38,7 @@ public:
 private:
   uint32_t lastId_ = 1;
 
+protected:
   std::vector<std::unique_ptr<graph::Node> > nodes_;
   std::vector<std::unique_ptr<graph::Edge> > edges_;
 };

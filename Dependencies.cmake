92aa138108e99a049bdade6adc2eb42412ed22b7
kasefuchs <kasefuchs@protonmail.com>
2026-01-10 17:21:40 +0300

    feat: add textchain train cli

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -4,6 +4,9 @@ find_package(protobuf CONFIG REQUIRED)
 # Abseil
 find_package(absl CONFIG REQUIRED)
 
+# CLI11
+find_package(CLI11 CONFIG REQUIRED)
+
 # pugixml
 find_package(pugixml CONFIG REQUIRED)
 

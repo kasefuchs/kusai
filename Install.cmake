f664973684e9049b190eb18e7b7766a3a2870334
kasefuchs <kasefuchs@protonmail.com>
2026-01-23 22:53:14 +0300

    remove: exclude well known dependencies

--- Install.cmake
+++ Install.cmake
@@ -18,6 +18,11 @@ endif ()
 
 install(RUNTIME_DEPENDENCY_SET ${PROJECT_NAME}
   DESTINATION ${CMAKE_INSTALL_LIBDIR}
+  PRE_EXCLUDE_REGEXES
+  "^ld-linux.*" "^libc\\.so" "^libm\\.so" "^libz\\.so" "^libdl\\.so"
+  "^libutil\\.so" "^libgcc_s\\.so" "^libstdc\\+\\+\\.so"
+  "^libpthread\\.so" "^librt\\.so" "^libresolv\\.so"
+  "^libcrypto\\.so" "^libssl\\.so" "^libsystemd\\.so"
 )
 
 include(CPack)

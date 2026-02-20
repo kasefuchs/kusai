1369a4a1aea1940c419ff5b4a093db6e9ed72bfe
kasefuchs <kasefuchs@protonmail.com>
2026-02-20 13:13:29 +0300

    :wrench: Add CMake helpers

--- CMake/Install.cmake
+++ CMake/Install.cmake
@@ -1,27 +1,41 @@
 include(GNUInstallDirs)
 
-install(TARGETS proto graph markov textchain tokenizer
+install(
+  TARGETS proto graph markov textchain tokenizer
   EXPORT kusai
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
   FILE_SET HEADERS DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
 if (KUSAI_BUILD_APP)
-  install(TARGETS kusai
+  install(
+    TARGETS kusai
     RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
   )
 endif ()
 
-configure_file("${CMAKE_CURRENT_LIST_DIR}/kusai-config.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/kusai-config.cmake" @ONLY)
-install(FILES "${CMAKE_CURRENT_BINARY_DIR}/kusai-config.cmake" DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/kusai")
+configure_file(
+  "${CMAKE_CURRENT_LIST_DIR}/kusai-config.cmake.in"
+  "${CMAKE_CURRENT_BINARY_DIR}/kusai-config.cmake"
+  @ONLY
+)
 
-install(EXPORT kusai
+install(
+  FILES "${CMAKE_CURRENT_BINARY_DIR}/kusai-config.cmake"
+  DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/kusai"
+)
+
+install(
+  EXPORT kusai
   FILE kusai-targets.cmake
+  NAMESPACE kusai::
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/kusai
 )
 
-install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/proto/kusai/proto
-        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/kusai
-        FILES_MATCHING PATTERN "*.pb.h")
+install(
+  DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/proto/kusai/proto
+  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/kusai
+  FILES_MATCHING PATTERN "*.pb.h"
+)
 
 include(CPack)

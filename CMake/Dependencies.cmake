b8df4cf02c50ea1e3794d65e19f8b57f8160dddc
kasefuchs <kasefuchs@protonmail.com>
2026-03-29 22:19:31 +0300

    feat: add python bindings

--- CMake/Dependencies.cmake
+++ CMake/Dependencies.cmake
@@ -1,3 +1,5 @@
+include(FetchContent)
+
 # Abseil
 find_package(absl CONFIG REQUIRED)
 
@@ -11,3 +13,15 @@ find_package(nlohmann_json CONFIG REQUIRED)
 
 # xxHash
 find_package(xxHash CONFIG REQUIRED)
+
+# Pybind11
+if (KUSAI_BUILD_PYTHON)
+  find_package(pybind11 CONFIG REQUIRED)
+
+  FetchContent_Declare(
+    pybind11_json
+    GIT_REPOSITORY https://github.com/pybind/pybind11_json.git
+    GIT_TAG        master
+  )
+  FetchContent_MakeAvailable(pybind11_json)
+endif()
